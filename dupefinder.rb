778d13028bd88b0f66bf769f87d5a8fb8c6248ea
karlcyr <karlcyr@gmail.com>
2012-12-04 09:50:24 -0800

    Cleaned up code, added CLI

--- dupefinder.rb
+++ dupefinder.rb
@@ -1,23 +1,38 @@
 #!/usr/bin/ruby
-
 # Check for duplicate image and video files
 
-# Define Statics
-debug = true
+require 'pathname'
 
-puts Dir.pwd if debug 
+# Define Statics
+debug = (ARGV[1] == 'debug')
+
+pwd = Dir.pwd
 starttime = Time.now
 
-# store entries for all files in the Pictures subdirectory with md5sum as key and filename as value.
+arg = ARGV[0] || Dir.pwd
+rootpath = Pathname.new(arg).realpath.to_s
+puts "Examining #{rootpath}"
+Dir.chdir(rootpath)
+
 filemap = Hash.new
 dupes = []
-filefinder = File.join("./","**","{.*,*}.{jpg,JPG,img,IMG,gif,GIF,mp4,MP4,mov,MOV,mpg,MPG,mpeg,MPEG}")
+filefinder = File.join("./","**","{.*,*}")
 files = Dir.glob(filefinder)
 print "files.size after collecting files: ", files.size, "\n" if debug
 
+count = 0
 files.each{ |file|
-	#http://developers.appoxy.com/2010/05/md5-hash-of-file-in-ruby.html
-	md5 = Digest::MD5.file(file).hexdigest
+	if Pathname.new(file).directory? then next end
+	if Pathname.new(file).size > (1024 * 1024 * 20) then 
+		print "Refusing to calculate digest of large file: #{file} \n"
+		next
+	 end
+	begin
+		md5 = Digest::MD5.file(file).hexdigest
+	rescue
+		next	
+	end
+
 	if filemap.key?(md5) 
 		dupe = []
 		dupe[0] = filemap.fetch md5
@@ -26,14 +41,20 @@ files.each{ |file|
 	else
 		filemap[md5] = file
 	end
-	
+	count += 1
+	print "Processed #{count} files in #{(Time.now-starttime)} seconds.\n" if debug && count%1000==0
 }
 
-print "filemap.size after processing: ", filemap.size, "\n" if debug
-print "dupes.size: ", dupes.size, "\n" if debug
+print "Total Files Scanned: #{count}\n"
+print "Unique Files: #{filemap.size}\n"
+print "Duplicate Files: ", dupes.size, "\n"
 
-File.open('duplicates.out','w') { |output|
-	output.write(dupes.inspect)
+outfile = "#{pwd}/duplicates.out"
+File.open(outfile,'w') { |output|
+	dupes.each { |dupe|
+		output.write("#{dupe[0]} |::| #{dupe[1]}\n")
+	}
 }
 
+print "Refer to duplicates.out for full list of duplicate files\n"
 print "Elapsed Time: ", (Time.now - starttime), " in seconds.\n" if debug
